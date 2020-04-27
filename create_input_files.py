0ec3c439a411c378debd031ad0b5122fcc27c2ec
jjmachan <jamesjithin97@gmail.com>
2020-04-27 18:48:17 +0530

    configured for flickr8k

--- create_input_files.py
+++ create_input_files.py
@@ -2,10 +2,13 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption data/',
+    json_path = '/home/jithin/datasets/imageCaptioning/captions/dataset_flickr8k.json'
+    img_folder = '/home/jithin/datasets/imageCaptioning/flicker8k/Flicker8k_Dataset'
+    output_folder = './pre_processed'
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path=json_path,
+                       image_folder=img_folder,
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption data/',
+                       output_folder=output_folder,
                        max_len=50)
