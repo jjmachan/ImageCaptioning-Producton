14db9ca760b112c5be3ccec0708c1193c39d0e86
jjmachan <jamesjithin97@gmail.com>
2020-04-30 21:49:57 +0530

    added comments and explainations

--- imageCaptioningService.py
+++ imageCaptioningService.py
@@ -5,7 +5,6 @@ import torch
 import torchvision
 from torchvision import transforms
 import torch.nn.functional as F
-
 import bentoml
 from bentoml.artifact import PytorchModelArtifact
 from bentoml.handlers import ImageHandler
@@ -25,6 +24,7 @@ word_map_file = 'word_map.json'
 word_map = json.load(open(word_map_file, 'r'))
 vocab_size = len(word_map)
 rev_word_map = {v:key for key, v in word_map.items()}
+
 @bentoml.env(pip_dependencies=['torch', 'torchvision'])
 @bentoml.artifacts([PytorchModelArtifact('encoder'), PytorchModelArtifact('decoder')])
 class ImageCaptioner(bentoml.BentoService):
@@ -32,28 +32,39 @@ class ImageCaptioner(bentoml.BentoService):
     @bentoml.api(ImageHandler)
     def gen_caption(self, img):
         k = beam_size
+
+        # Get the image
         img = Image.fromarray(img)
         img = transform(img)
+        print(img.shape)
         img = img.unsqueeze(0)
 
         # Pass to encoder
         self.artifacts.encoder.eval()
+        # (1, enc_img_size, enc_img_size, encoder_dim)
         encoder_out = self.artifacts.encoder(img)
         enc_img_size = encoder_out.size(1)
         encoder_dim = encoder_out.size(3)
 
+        # (1, enc_img_size*enc_img_size, encoder_dim)
         encoder_out = encoder_out.view(1, -1, encoder_dim)
         num_pixels = encoder_out.size(1)
 
+        # we are treating the problem as having a batch size of k
         encoder_out = encoder_out.expand(k, num_pixels, encoder_dim)
-        print(encoder_out.shape)
 
+        # Tensor to store top k previous word at each step
+        # (k, 1)
         k_prev_words = torch.LongTensor([[word_map['<start>']]] * k)
-        seqs = k_prev_words
-        top_k_scores = torch.zeros(k, 1)
-        seqs_alpha = torch.ones(k, 1, enc_img_size, enc_img_size)
 
-        # list to store completed seqs, alpha and scores
+        # Tensor to store top k sequences; now they're just <start>
+        # (k, 1)
+        seqs = k_prev_words
+
+        # Tensor to keep track of the scores of each seq
+        top_k_scores = torch.zeros(k, 1)
+
+        # list to store completed seqs and scores
         complete_seqs = list()
         complete_seqs_alpha = list()
         complete_seqs_scores = list()
@@ -63,30 +74,39 @@ class ImageCaptioner(bentoml.BentoService):
         decoder = self.artifacts.decoder.eval()
         h, c = decoder.init_hidden_state(encoder_out)
 
+        # s is a number less than or equal to k, because sequences are removed
+        # from this process once they hit <end>
         while True:
 
             # (s, embed_dim)
             embeddings = decoder.embedding(k_prev_words).squeeze(1)
             # (s, encoder_dim) (s, num_pixels)
-            awe, alpha = decoder.attention(encoder_out, h)
+            awe, _ = decoder.attention(encoder_out, h)
 
-            alpha = alpha.view(-1, enc_img_size, enc_img_size)
 
+            # Gating scalar
+            # (s, encoder_dim)
             gate = decoder.sigmoid(decoder.f_beta(h))
             awe = gate*awe
 
+            # (s, decoder_dim)
             h, c = decoder.decode_step(torch.cat([embeddings, awe], dim=1), (h,c))
 
+            # (s, vocab_size)
             scores = decoder.fc(h)
             scores = F.log_softmax(scores, dim=1)
 
+            # Add
             scores = top_k_scores.expand_as(scores) + scores
 
             if step == 1:
+                # if first step then all the words are same
                 top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)
             else:
+                # else unroll and get the top k scores and word indexes
                 top_k_scores, top_k_words = scores.view(-1).topk(k, 0, True, True)
 
+            # convert the unrolled indices to actual indices of scores
             prev_word_inds = top_k_words / vocab_size
             next_word_inds = top_k_words % vocab_size
 
@@ -94,6 +114,7 @@ class ImageCaptioner(bentoml.BentoService):
             # (s, step+1)
             seqs = torch.cat([seqs[prev_word_inds], next_word_inds.unsqueeze(1)], dim=1)
 
+            # the seq that did not reach <end>
             incomplete_inds = [ind for ind, next_word in enumerate(next_word_inds) if
                                next_word != word_map['<end>']]
             complete_inds = list(set(range(len(next_word_inds))) - set(incomplete_inds))
@@ -107,7 +128,6 @@ class ImageCaptioner(bentoml.BentoService):
             # Proceed with incomplete sequences
             if k == 0:
                 break
-
             seqs = seqs[incomplete_inds]
             h = h[prev_word_inds[incomplete_inds]]
             c = c[prev_word_inds[incomplete_inds]]
